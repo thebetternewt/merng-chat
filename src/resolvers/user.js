9d7c857712bd62c4081a5cb152e7fa80b67dd08d
Chris Eady <chris.eady1@gmail.com>
2019-01-25 12:20:10 -0600

    Work on authorization

--- src/resolvers/user.js
+++ src/resolvers/user.js
@@ -6,8 +6,14 @@ import { signUp } from '../validation';
 
 export default {
   Query: {
-    users: async (root, args, context, info) => User.find({}),
+    users: async (root, args, context, info) => {
+      checkSignedIn()
+
+      return User.find({})
+    },
     user: async (root, { id }, context, info) => {
+      checkSignedIn()
+
       if (!mongoose.Types.ObjectId.isValid(id)) {
         throw new UserInputError('User not found.')
       }
@@ -17,6 +23,8 @@ export default {
   },
   Mutation: {
     signUp: async (root, args, context, info) => {
+      checkSignedOut()
+
       await Joi.validate(args, signUp, { abortEarly: false })
 
       return User.create(args)
